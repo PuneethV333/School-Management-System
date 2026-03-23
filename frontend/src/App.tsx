d2392353c96bb1dd10778f819fe1bfd446f9eea4
Puneeth V <puneethvijayakumar333@gmail.com>
2026-03-23 21:14:58 +0530

    login-complete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,36 @@
-const App = () => {
-    
-    
-  return (
-    <div>App</div>
-  )
-}
+import { lazy, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+import { useFetchMe } from "./hooks/useAuth";
+import Spinner from "./components/Spinner";
+import Home from "./pages/Home";
 
-export default App
+
+
+const App = () => {
+  const Login = lazy(() => import("./pages/Login"));
+  
+  
+  
+  const { isPending, data: userData } = useFetchMe();
+  
+  if(userData){
+    console.log(userData);
+  }
+  
+  if(isPending){
+    return <Spinner/>
+  }
+
+  return (
+    <>
+      <Suspense>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Suspense>
+    </>
+  );
+};
+
+export default App;
