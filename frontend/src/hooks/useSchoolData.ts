f1f6bc019e1e8be119f48327e01fed6b5838362d
Puneeth V <puneethvijayakumar333@gmail.com>
2026-03-24 15:26:39 +0530

    updated-useSchool-Data

--- frontend/src/hooks/useSchoolData.ts
+++ frontend/src/hooks/useSchoolData.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchSchoolInfo } from "../api/school.api";
 
 export const useSchoolData = (userData:object)  => useQuery({
-    queryKey:["school"],
+    queryKey:["school",userData],
     queryFn:fetchSchoolInfo,
     select:(res) => (res.data),
     retry:false,
