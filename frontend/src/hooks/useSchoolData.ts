2f4ab0fe8eab4599068a9b48f39c76298122c50f
Puneeth V <puneethvijayakumar333@gmail.com>
2026-03-24 18:46:16 +0530

    updated-userData-type

--- frontend/src/hooks/useSchoolData.ts
+++ frontend/src/hooks/useSchoolData.ts
@@ -1,10 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchSchoolInfo } from "../api/school.api";
 
-export const useSchoolData = (userData:object)  => useQuery({
+type userData = {
+    role?:'student'|'authority'|'teacher'
+    name?:string
+}
+
+export const useSchoolData = (userData:userData)  => useQuery({
     queryKey:["school",userData],
     queryFn:fetchSchoolInfo,
     select:(res) => (res.data),
     retry:false,
-    enabled:!!userData
+    enabled:!!userData?.role
 })
