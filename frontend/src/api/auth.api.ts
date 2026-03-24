34ad8aa85391d217a1c03397b68d92ae4b4ebe73
Puneeth V <puneethvijayakumar333@gmail.com>
2026-03-24 14:14:35 +0530

    added-menu-bar

--- frontend/src/api/auth.api.ts
+++ frontend/src/api/auth.api.ts
@@ -7,3 +7,6 @@ export interface loginBody {
 
 export const loginAPI = (payload:loginBody) => api.post("/auth/login", payload).then(res => res.data);
 export const fetchMeAPI = () => api.get("/auth/getMe").then(res => res.data);
+export const logoutAPI = () => api.post("/auth/logout").then(() => true);
+export const changePasswordAPI = (oldPass:string, newPass:string) =>
+  api.post("/api/user/changePass", { oldPass, newPass }).then(res => res.data);
--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,6 +1,12 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { fetchMeAPI, loginAPI } from "../api/auth.api";
+import {
+  changePasswordAPI,
+  fetchMeAPI,
+  loginAPI,
+  logoutAPI,
+} from "../api/auth.api";
 import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 export const useLogin = () => {
   const queryClient = useQueryClient();
@@ -24,5 +30,44 @@ export const useFetchMe = () => {
     },
     retry: false,
     staleTime: 5 * 60 * 1000,
+  });
+};
+
+export const useLogout = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  return useMutation({
+    mutationFn: logoutAPI,
+
+    onSuccess: () => {
+      queryClient.setQueryData(["me"], null);
+      queryClient.clear();
+      navigate("/login", { replace: true });
+    },
+
+    onError: () => {
+      toast.error("Logout failed");
+    },
+  });
+};
+
+type useChangePasswordProps = {
+  oldPass: string;
+  newPass: string;
+};
+
+export const useChangePassword = () => {
+  return useMutation({
+    mutationFn: ({ oldPass, newPass }: useChangePasswordProps) =>
+      changePasswordAPI(oldPass, newPass),
+
+    onSuccess: (res) => {
+      toast.success(res.msg || "Password changed successfully");
+    },
+
+    onError: (err) => {
+      toast.error(err?.message || "Failed to change password");
+    },
   });
 };
