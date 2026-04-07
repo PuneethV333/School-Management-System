4527ef4cb0f406f724721331f5510efd124ee971
Puneeth V <puneethvijayakumar333@gmail.com>
2026-04-07 14:23:21 +0530

    completed-add-announcement-via-cloudenary

--- frontend/src/utils/cloudinaryHelper.ts
+++ frontend/src/utils/cloudinaryHelper.ts
@@ -1,22 +1,17 @@
 import axios from "axios"
 
-export const returnUrl = async (file:File) => {
-    const cloudinary_url = `https://api.cloudinary.com/v1_1/${
-        import.meta.env.VITE_CLOUDINARY_CLOUD_NAME
-    }/image/upload`
-    
-    
-    console.log(import.meta.env.VITE_CLOUDINARY_CLOUD_NAME);
-console.log(import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET);
-    
+export const returnUrl = async (file: File) => {
+    const cloudinary_url = `https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUDINARY_CLOUD_NAME
+        }/image/upload`
+
     const data = new FormData()
-    data.append('file',file)
-    data.append('upload_preset',import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET)
-    const res = await axios.post(cloudinary_url,data,{
-        headers:{
-            "Content-Type":"multipart/form-data"
+    data.append('file', file)
+    data.append('upload_preset', import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET)
+    const res = await axios.post(cloudinary_url, data, {
+        headers: {
+            "Content-Type": "multipart/form-data"
         }
     });
-    
+
     return res?.data?.secure_url;
 }
